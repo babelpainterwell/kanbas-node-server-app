ba1e4f6ad83301e0b9f73e56f55aa59cdc086bf4
babelpainterwell <zhang.zhongw@northeastern.edu>
2023-12-04 23:38:49 -0500

    first commit

--- app.js
+++ app.js
@@ -5,11 +5,31 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
 const app = express();
 // app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
