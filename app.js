300ee40f3e88efc13184d51f97b0385a19cda2f0
babelpainterwell <zhang.zhongw@northeastern.edu>
2023-12-11 19:28:12 -0500

    update for debugging deployment

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-import "dotenv/config";
 import mongoose from "mongoose";
-const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
-mongoose.connect(CONNECTION_STRING);
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
 import "dotenv/config";
+
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 // app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
   })
 );
 const sessionOptions = {
@@ -26,6 +26,13 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
 app.use(session(sessionOptions));
 
 app.use(express.json());
